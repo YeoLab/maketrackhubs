06df095d0872dc56f171e8db712c702d4dd99ee5
Brian Yee <bay001@ucsd.edu>
2019-03-05 16:48:08 -0800

    v0.0.2 maketrackhubs first effective commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # maketrackhubs
+Current version: 0.0.2
+
+# you need to have an aws profile called "yeolab-trackhubs"
+
+contact me (Brian) if you need credentials
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='maketrackhubs',
+    version='0.0.2',
+    url='github.com/YeoLab/maketrackhubs',
+    license='',
+    author='gpratt, adomissy, byee4',
+    author_email='bay001@ucsd.edu',
+    description='Creates and attempts to upload trackhub files to yeolab aws',
+    packages=['maketrackhubs'],
+    package_dir={
+        'maketrackhubs': 'maketrackhubs',
+    },
+    entry_points = {
+        'console_scripts': [
+            'maketrackhubs = maketrackhubs.make_trackhubs:main',
+        ]
+    }
+)
